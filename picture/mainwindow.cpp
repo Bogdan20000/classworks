757c07ac514589b29add03374b2e919aba467d44
Bogdan 20000 <bacola2501@gmail.com>
2019-05-02 19:56:53 +0300

    Slideshow project

--- picture/mainwindow.cpp
+++ picture/mainwindow.cpp
@@ -1,13 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <iostream>
+#include <string>
+#include <QKeyEvent>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setPic();
 }
 
 MainWindow::~MainWindow()
@@ -15,70 +16,52 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::setPic()
+void MainWindow::setPic(QString a)
 {
     QPixmap pic = QPixmap();
-    if(pic.load("C:\\Users\\Admin\\Desktop\\1459849923_prikol-1.jpg")){
-        pic = pic.scaled(ui->label->size(), Qt::KeepAspectRatio);
-        ui->label->setPixmap(pic);
+        if(pic.load(a)){
+            std::cout << currentImage;
+            pic = pic.scaled(ui->label->size(), Qt::KeepAspectRatio);
+            ui->label->setPixmap(pic);
+        }
+        else{
+            std::cout << "Eror" << std::endl;
+        }
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    if(event->key() == Qt::Key_Left){
+        std::cout << "left" << std :: endl;
+        currentImage --;
+        if (currentImage < 0){
+            currentImage = 9;
+        }
+        setPic(image [currentImage]);
+
     }
-    else{
-        std::cout << "fail";
+    else if(event->key() == Qt::Key_Right){
+        std::cout << "right" << std :: endl;
+        currentImage ++;
+        if (currentImage > 9){
+            currentImage = 0;
+        }
+        setPic(image [currentImage]);
     }
-    if(pic.load("C:\\Users\\Admin\\Desktop\\152027388416666899.jpg")){
-        pic = pic.scaled(ui->label_2->size(), Qt::KeepAspectRatio);
-        ui->label_2->setPixmap(pic);
+    else if(event->key() == Qt::Key_Up){
+        std::cout << "up" << std :: endl;
+        currentImage ++;
+        if (currentImage > 9){
+            currentImage = 0;
+        }
+        setPic(image [currentImage]);
     }
-    else{
-        std::cout << "fail";
-    }
-    if(pic.load("C:\\Users\\Admin\\Desktop\\images.jpg")){
-        pic = pic.scaled(ui->label_3->size(), Qt::KeepAspectRatio);
-        ui->label_3->setPixmap(pic);
-    }
-    else{
-        std::cout << "fail";
-    }
-    if(pic.load("C:\\Users\\Admin\\Desktop\\images (1).jpg")){
-        pic = pic.scaled(ui->label_4->size(), Qt::KeepAspectRatio);
-        ui->label_4->setPixmap(pic);
-    }
-    else{
-        std::cout << "fail";
-    }
-    if(pic.load("C:\\Users\\Admin\\Desktop\\1402471536_prikoly-6.jpg")){
-        pic = pic.scaled(ui->label_5->size(), Qt::KeepAspectRatio);
-        ui->label_5->setPixmap(pic);
-    }
-    else{
-        std::cout << "fail";
-    }
-    if(pic.load("C:\\Users\\Admin\\Desktop\\images (2).jpg")){
-        pic = pic.scaled(ui->label_6->size(), Qt::KeepAspectRatio);
-        ui->label_6->setPixmap(pic);
-    }
-    else{
-        std::cout << "fail";
-    }
-    if(pic.load("C:\\Users\\Admin\\Desktop\\1470283727_001_4.jpg")){
-        pic = pic.scaled(ui->label_7->size(), Qt::KeepAspectRatio);
-        ui->label_7->setPixmap(pic);
-    }
-    else{
-        std::cout << "fail";
-    }
-    if(pic.load("C:\\Users\\Admin\\Desktop\\3276bbc8f0271d698aeda63f38844857.jpg")){
-        pic = pic.scaled(ui->label_8->size(), Qt::KeepAspectRatio);
-        ui->label_8->setPixmap(pic);
-    }
-    else{
-        std::cout << "fail";
-    }
-    if(pic.load("C:\\Users\\Admin\\Desktop\\kartinki-s-nadpisjami-3_tumb_660.jpg")){
-        pic = pic.scaled(ui->label_9->size(), Qt::KeepAspectRatio);
-        ui->label_9->setPixmap(pic);
-    }
-    else{
-        std::cout << "fail";
+    else if(event->key() == Qt::Key_Down){
+        std::cout << "down" << std :: endl;
+        currentImage --;
+        if (currentImage < 0){
+            currentImage = 9;
+        }
+        setPic(image [currentImage]);
     }
 }
--- picture/mainwindow.h
+++ picture/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
 
 namespace Ui {
 class MainWindow;
@@ -14,7 +15,22 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    void setPic();
+    void setPic(QString a);
+    void keyPressEvent(QKeyEvent *event);
+    QString image [10] = {
+        "C://Users//Admin//Desktop//QT_QT_QT_QT//3276bbc8f0271d698aeda63f38844857.jpg",
+        "C://Users//Admin//Desktop//QT_QT_QT_QT//1402471536_prikoly-6.jpg",
+        "C://Users//Admin//Desktop//QT_QT_QT_QT//1459849923_prikol-1.jpg",
+        "C://Users//Admin//Desktop//QT_QT_QT_QT//1470283727_001_4.jpg",
+        "C://Users//Admin//Desktop//QT_QT_QT_QT//152027388416666899.jpg",
+        "C://Users//Admin//Desktop//QT_QT_QT_QT//images (1).jpg",
+        "C://Users//Admin//Desktop//QT_QT_QT_QT//images (2).jpg",
+        "C://Users//Admin//Desktop//QT_QT_QT_QT//images.jpg",
+        "C://Users//Admin//Desktop//QT_QT_QT_QT//kartinki-s-nadpisjami-3_tumb_660.jpg",
+        "C://Users//Admin//Desktop//QT_QT_QT_QT//завантаження.jpg"
+    };
+    int currentImage = 0;
+    int numOfImages = 9;
 
 private:
     Ui::MainWindow *ui;
