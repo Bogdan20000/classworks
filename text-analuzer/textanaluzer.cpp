f0a6a70c17ebe9223d578046363b5bc782d29835
Bogdan 20000 <bacola2501@gmail.com>
2019-05-22 19:29:22 +0300

    Added text analyzer functions

--- text-analuzer/textanaluzer.cpp
+++ text-analuzer/textanaluzer.cpp
@@ -12,27 +12,64 @@ void textanaluzer::setText(string text)
 
 int textanaluzer::getlettersCount()
 {
-    return 100;
+    int a = text.length();
+    int z = 0;
+    for(int i = 0; i<a; i ++){
+        if (!(text[i] > 64 && text[i] < 91|| text[i] > 96 && text[i] < 123)){
+            z++;
+        }
+    }
+    return a - z;
 }
 
 int textanaluzer::getWordsCount()
 {
     int z = 0;
-    for(int i = 0; i<1000; i ++){
-        if (text[i] == 32){
-            z++
+    int a = 0;
+    a = text.length();
+    for(int i = 0; i<a; i ++){
+        if (text[i] == 32|| text[i] == 39|| text[i] == 44|| text[i] == 62|| text[i] == 47|| text[i] == 61|| text[i] == 33){
+            if (!(i == 0)){
+                if (text[i-1] > 64 && text[i-1] < 91|| text[i-1] > 96 && text[i-1] < 123||text[i-1] == 44|| text[i-1] == 46|| text[i-1] == 33|| text[i-1] == ':'|| text[i-1] == 34|| text[i-1] == 59) {
+                    if(text[i+1] > 64 && text[i+1] < 91|| text[i+1] > 96 && text[i+1] < 123|| text[i+1] == 32){
+                        z++;
+                    }
+                }
+            }
         }
     }
+    for(int i = 0; i<a; i ++){
+        if (text[i] == 33 && (text[i+1] > 64 && text[i+1] < 91|| text[i+1] > 96 && text[i+1] < 123)){
+            z++;
+        }
+    }
+
+    if (a > 1){
+        z++;
+    }
+    return z;
 }
 
 int textanaluzer::getSymbolsCount()
 {
-    return 100;
+    int a = text.length();
+    return a;
 }
 
 int textanaluzer::getSentencesCount()
 {
-    return 100;
+    int z = 0;
+    int a = 0;
+    a = text.length();
+    for(int i = 0; i<a; i++){
+        if (!(text[i] > 64 && text[i] < 91|| text[i] > 96 && text[i] < 123)){
+            z++;
+        }
+    }
+    if (z < 1){
+        z--;
+    }
+    return z;
 }
 
 string textanaluzer::changeWords(string previous, string newWorld)
